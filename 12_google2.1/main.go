26960425eb3ca345525b6ef05659ab35e1e4c980
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-02-27 22:06:48 +0100

    Google 2.1

---
+++ 12_google2.1/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Result of search.
+type Result string
+
+// Search function.
+type Search func(query string) Result
+
+// Google invokes Web, Image and Video searches for query concurrently.
+func Google(query string) (results []Result) {
+	ch := make(chan Result)
+	// Let's speed things up by adding goroutines.
+	go func() { ch <- Web(query) }()
+	go func() { ch <- Image(query) }()
+	go func() { ch <- Video(query) }()
+	for i := 0; i < 3; i++ {
+		result := <-ch
+		results = append(results, result)
+	}
+	return
+}
+
+// Various search kinds.
+var (
+	Web   = fakeSearch("web")
+	Image = fakeSearch("image")
+	Video = fakeSearch("video")
+)
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100) * int(time.Millisecond)))
+		return Result(fmt.Sprintf("%s result for %q", kind, query))
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	results := Google("golang")
+	elapsed := time.Since(start)
+	fmt.Println(results)
+	fmt.Println(elapsed)
+}
