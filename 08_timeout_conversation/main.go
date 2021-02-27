3d3d79c1711b88ccfb01ed0ce1eb58c048c4a965
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-02-27 13:41:03 +0100

    timeout the whole conversation

---
+++ 08_timeout_conversation/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	ch := boring("boring!")
+	timeout := time.After(3 * time.Second)
+	for {
+		select {
+		case s := <-ch:
+			fmt.Println(s)
+		case <-timeout:
+			fmt.Println("You talk too much.")
+			return
+		}
+	}
+}
+
+func boring(msg string) <-chan string {
+	ch := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			ch <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return ch
+}
