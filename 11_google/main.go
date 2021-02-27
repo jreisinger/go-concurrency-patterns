848cf680c46ce3f129a70bc1f29df2f31226a1e6
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-02-27 21:11:46 +0100

    Google search engine

---
+++ 11_google/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Result of search.
+type Result string
+
+// Google text, image and video results of the query.
+func Google(query string) (results []Result) {
+	results = append(results, Web(query))
+	results = append(results, Image(query))
+	results = append(results, Video(query))
+	return
+}
+
+// Various search kinds.
+var (
+	Web   = fakeSearch("web")
+	Image = fakeSearch("image")
+	Video = fakeSearch("video")
+)
+
+// Search function.
+type Search func(query string) Result
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		time.Sleep(time.Duration(rand.Intn(100) * int(time.Millisecond)))
+		return Result(fmt.Sprintf("%s result for %q", kind, query))
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	results := Google("golang")
+	elapsed := time.Since(start)
+	fmt.Println(results)
+	fmt.Println(elapsed)
+}
