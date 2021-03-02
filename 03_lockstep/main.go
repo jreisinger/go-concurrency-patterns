09dd8f421de4646418610b449b99745adaa54bc6
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-03-02 10:01:43 +0100

    minor changes in 03_lockstep/main.go

--- 03_lockstep/main.go
+++ 03_lockstep/main.go
@@ -1,3 +1,6 @@
+// Get two channels from boring() and print from them in sequence (lockstep).
+
+// Channels as a handle on a service.
 package main
 
 import (
@@ -7,25 +10,23 @@ import (
 )
 
 func main() {
-	// Channels as a handle on a service
 	joe := boring("Joe")
 	ann := boring("Ann")
 
 	for i := 0; i < 5; i++ {
-		// joe and ann count in lockstep (sequence)
 		fmt.Println(<-joe)
 		fmt.Println(<-ann)
 	}
-	fmt.Println("You're boring; I'm returning.")
+	fmt.Println("I'm returning.")
 }
 
 func boring(msg string) <-chan string {
-	ch := make(chan string)
+	c := make(chan string)
 	go func() {
 		for i := 0; ; i++ {
-			ch <- fmt.Sprintf("%s %d", msg, i)
+			c <- fmt.Sprintf("%s %d", msg, i)
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 		}
 	}()
-	return ch
+	return c
 }
