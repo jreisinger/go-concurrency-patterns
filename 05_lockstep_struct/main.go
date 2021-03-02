00d9110997e9550cbcab4db630f0b1cb92c48032
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-03-02 18:35:56 +0100

    minor changes in 05_lockstep_struct/main.go

--- 05_lockstep_struct/main.go
+++ 05_lockstep_struct/main.go
@@ -1,3 +1,5 @@
+// Let's put sequencing back but in different way. Each speaker must wait for a
+// go-ahead.
 package main
 
 import (
@@ -12,42 +14,39 @@ type message struct {
 }
 
 func main() {
-	ch := fanIn(boring("Joe"), boring("Ann"))
+	c := fanIn(boring("Joe"), boring("Ann"))
 	for i := 0; i < 5; i++ {
-		msg1 := <-ch
+		msg1 := <-c
 		fmt.Println(msg1.str)
-		msg2 := <-ch
+		msg2 := <-c
 		fmt.Println(msg2.str)
-
-		// Each speaker must wait for a go-ahead.
 		msg1.wait <- true
 		msg2.wait <- true
 	}
-	fmt.Println("You're boring; I'm returning.")
 }
 
 func fanIn(inputs ...<-chan message) <-chan message {
-	ch := make(chan message)
+	c := make(chan message)
 	for i := range inputs {
 		input := inputs[i] // new instance of 'input' for each loop
 		go func() {
 			for {
-				ch <- <-input
+				c <- <-input
 			}
 		}()
 	}
-	return ch
+	return c
 }
 
 func boring(msg string) <-chan message {
-	ch := make(chan message)
+	c := make(chan message)
 	waitForIt := make(chan bool) // shared between all messages
 	go func() {
 		for i := 0; ; i++ {
-			ch <- message{fmt.Sprintf("%s %d", msg, i), waitForIt}
+			c <- message{fmt.Sprintf("%s %d", msg, i), waitForIt}
 			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 			<-waitForIt
 		}
 	}()
-	return ch
+	return c
 }
