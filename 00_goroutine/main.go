632d9b71e92841a047ac3091b57b7025ec7eac62
Jozef Reisinger <jozef.reisinger@pan-net.eu>
2021-03-06 12:03:55 +0100

    goroutine w/o channel

---
+++ 00_goroutine/main.go
@@ -0,0 +1,21 @@
+// Launch boring goroutine let it talk a bit and exit. There's no communication.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	go boring("boring!")
+	time.Sleep(time.Second * 2)
+	fmt.Println("I'm returning.")
+}
+
+func boring(msg string) {
+	for i := 0; ; i++ {
+		fmt.Printf("%s %d\n", msg, i)
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	}
+}
